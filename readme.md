bdbba43753859bbe0627c938c17df716759ce172
Sebastian Laube <sebastian.laube@zebra.de>
2024-04-02 17:29:44 +0200

    intial commit

---
+++ readme.md
@@ -0,0 +1,55 @@
+# `<slide-roll>`
+
+Simple (non interactive) slide show. You know these things designers use to prevent bloodsheds in management meetings…
+
+It provides basic functionality, by moving through the slides in a given interval. No separate dependencies, only modern CSS and little JavaScrip logic.
+
+[Check out the Demo](https://bitstarr.github.io/slide-roll/demo.html)
+
+## Usage
+
+```html
+<script type="module" src="slide-roll.js"></script>
+<link rel="stylesheet" href="slide-roll.css">
+
+<slide-roll class="slide-roll" interval="4">
+    <div data-track>
+        <div class="stage">
+            <img
+                class="image"
+                src="https://picsum.photos/333/500?random=1"
+                width=960
+                height=540
+                alt="Random Image from Unsplash"
+            >
+            <p>
+                Fancy call to action text?
+            </p>
+        </div>
+        <div class="ks-item">
+            <img
+                class="image"
+                src="https://picsum.photos/333/500?random=2"
+                width=960
+                height=540
+                alt="Another random Image from Unsplash"
+            >
+            <p>
+                Have some more text?
+            </p>
+        </div>
+        […]
+    </div>
+    <div data-indicator></div>
+</slide-roll >
+```
+
+Basic structure and styling comes from the slide-roll CSS, which targets the `.slide-roll` class and the data attributes for the track and indicator.
+
+Inside the slides you can do whatever you want. The slides will be the full width of the slide-roll. The height of all slides needs to be the same. Inside you can put only an image or create fancy layout with a bunch of HTML elements and use flexbox, grids or absolute positioning.
+
+The interval can be set via the `interval` attribute and is given in seconds.
+
+The slideshow will pause if the user hovers (mouseover) or touches the element. You can set the attribute `nonstop` to the slide-roll, to prevent pausing.
+
+The indicator is optional. Simply remove the div from the example. You can customize the design of the indication display (look at the demo) since the default styling has a very low specificy.
---
+++ slide-roll.js
@@ -0,0 +1,151 @@
+class SlideRoll extends HTMLElement
+{
+    static tagName = 'slide-roll';
+    static track = null;
+    static interval = null;
+
+    constructor()
+    {
+        super();
+        this.checkEdge = this.checkEdge.bind(this);
+    }
+
+    connectedCallback()
+    {
+        // collect some information for later
+        this.track = this.querySelector( '[data-track]' );
+        this.interval = this.getAttribute( 'interval' ) || 5;
+        this.indicator = this.querySelector( '[data-indicator]' );
+        this.entryCount = this.track.children.length;
+        this.entryPointer = 1;
+
+        if ( this.entryCount == 1 )
+        {
+            // nothing to do here.
+            return;
+        }
+
+        if ( this.indicator )
+        {
+            this.mountIndicator();
+        }
+
+        // make it pauseable
+        this.pause = false;
+        if ( this.getAttribute( 'nonstop' ) === null )
+        {
+            this.addEventListener( 'mouseenter', this.stop );
+            this.addEventListener( 'touchstart', this.stop );
+            this.addEventListener( 'mouseleave', this.play );
+            this.addEventListener( 'touchend', this.play );
+        }
+
+        // now run!
+        this.runCarousel();
+    }
+
+    runCarousel()
+    {
+        // clone for the effect
+        const first = this.track.firstElementChild.cloneNode( true );
+        this.track.appendChild( first );
+
+        // move to first position, to be sure…
+        this.rewind();
+
+        // run
+        const runner = setInterval( function( self )
+            {
+                // but pause on hover
+                if ( ! self.pause )
+                {
+                    self.move();
+                    self.setIndicator();
+                }
+            },
+            this.interval * 1000,
+            this
+        );
+
+        // check if we reached the end
+        this.track.addEventListener( 'scrollend', this.checkEdge );
+    }
+
+    stop()
+    {
+        this.pause = true;
+    }
+
+    play()
+    {
+        this.pause = false;
+    }
+
+    move() {
+        this.track.scroll({
+            top: 0,
+            left: this.track.clientWidth * this.entryPointer,
+            behavior: 'auto',
+        });
+        this.entryPointer++;
+    }
+
+    rewind()
+    {
+        this.entryPointer = 1;
+        this.track.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'instant',
+        });
+    }
+
+    checkEdge()
+    {
+        // on last element scroll to first
+        if ( this.entryPointer > this.entryCount )
+        {
+            this.rewind()
+        }
+    }
+
+    mountIndicator()
+    {
+        for ( let i = 0; i < this.entryCount; i++ ) {
+            const item = document.createElement( 'span' );
+            this.indicator.appendChild( item );
+        }
+        this.indicator.firstChild.dataset.active = true;
+    }
+
+    setIndicator()
+    {
+        if ( !this.indicator )
+        {
+            return;
+        }
+
+        const pointer = ( this.entryPointer > this.entryCount ) ? 1 : this.entryPointer;
+        const items = this.indicator.children;
+        for ( var i = 0; i < items.length; i++ )
+        {
+            const item = items[i];
+            const key = i + 1;
+            delete item.dataset.active;
+
+            // set active if key and pointer match
+            // or it's going to be the cloned slide
+            if (
+                    key == this.entryPointer ||
+                    ( this.entryPointer > this.entryCount && i === 0 )
+                )
+            {
+                item.dataset.active = true;
+            }
+        }
+    }
+}
+
+if ( 'customElements' in window ) {
+	customElements.define( SlideRoll.tagName, SlideRoll );
+}
